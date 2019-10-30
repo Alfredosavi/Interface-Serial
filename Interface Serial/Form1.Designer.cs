4b270a6f2507c042df3e95725822892c9ddd2ac1
Alfredo Savi <alfredosavi@hotmail.com>
2019-10-30 01:03:56 -0300

    Adicionar arquivos de projeto.

---
+++ Interface Serial/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Interface_Serial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bt_conectar = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.bt_enviar = new System.Windows.Forms.Button();
+            this.textBox_Enviar = new System.Windows.Forms.TextBox();
+            this.textBox_Recebido = new System.Windows.Forms.TextBox();
+            this.timerPCOM = new System.Windows.Forms.Timer(this.components);
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.limpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bt_conectar
+            // 
+            resources.ApplyResources(this.bt_conectar, "bt_conectar");
+            this.bt_conectar.BackColor = System.Drawing.Color.Green;
+            this.bt_conectar.Name = "bt_conectar";
+            this.bt_conectar.UseVisualStyleBackColor = false;
+            this.bt_conectar.Click += new System.EventHandler(this.Bt_conectar_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            resources.ApplyResources(this.comboBox1, "comboBox1");
+            this.comboBox1.Name = "comboBox1";
+            // 
+            // bt_enviar
+            // 
+            resources.ApplyResources(this.bt_enviar, "bt_enviar");
+            this.bt_enviar.Name = "bt_enviar";
+            this.bt_enviar.UseVisualStyleBackColor = true;
+            this.bt_enviar.Click += new System.EventHandler(this.Bt_enviar_Click);
+            // 
+            // textBox_Enviar
+            // 
+            resources.ApplyResources(this.textBox_Enviar, "textBox_Enviar");
+            this.textBox_Enviar.Name = "textBox_Enviar";
+            // 
+            // textBox_Recebido
+            // 
+            resources.ApplyResources(this.textBox_Recebido, "textBox_Recebido");
+            this.textBox_Recebido.Name = "textBox_Recebido";
+            // 
+            // timerPCOM
+            // 
+            this.timerPCOM.Interval = 900;
+            this.timerPCOM.Tick += new System.EventHandler(this.TimerPCOM_Tick);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.SerialPort1_DataReceived);
+            // 
+            // limpar
+            // 
+            resources.ApplyResources(this.limpar, "limpar");
+            this.limpar.Name = "limpar";
+            this.limpar.UseVisualStyleBackColor = true;
+            this.limpar.Click += new System.EventHandler(this.Limpar_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.limpar);
+            this.Controls.Add(this.textBox_Recebido);
+            this.Controls.Add(this.textBox_Enviar);
+            this.Controls.Add(this.bt_enviar);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.bt_conectar);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_conectar;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button bt_enviar;
+        private System.Windows.Forms.TextBox textBox_Enviar;
+        private System.Windows.Forms.TextBox textBox_Recebido;
+        private System.Windows.Forms.Timer timerPCOM;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button limpar;
+    }
+}
+
---
+++ Interface Serial/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Interface_Serial
+{
+    public partial class Form1 : Form
+    {
+        string Rx_Global;
+        public Form1()
+        {
+            InitializeComponent();
+            timerPCOM.Enabled = true;
+        }
+
+        private void AtualizaPortasCOM()
+        {
+            int i = 0;
+            bool atualiza = false;
+
+            if (comboBox1.Items.Count == SerialPort.GetPortNames().Length)
+            {
+                foreach (string a in SerialPort.GetPortNames())
+                {
+                    if (comboBox1.Items[i++].Equals(a) == false)
+                    {
+                        atualiza = true;
+                    }
+                }
+            }
+            else
+            {
+                atualiza = true;
+            }
+
+            if (atualiza == false)
+            {
+                return;
+            }
+
+            comboBox1.Items.Clear(); // limpar o role
+            foreach (string a in SerialPort.GetPortNames()) // adicionando as portas COM novas
+            {
+                comboBox1.Items.Add(a);
+            }
+            comboBox1.SelectedIndex = 0; //selecionar a primeira porta que aparecer na lista
+        }
+
+        private void TimerPCOM_Tick(object sender, EventArgs e)
+        {
+            AtualizaPortasCOM();
+        }
+
+        private void Bt_conectar_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen == false)
+            {
+                try
+                {
+                    serialPort1.PortName = comboBox1.Items[comboBox1.SelectedIndex].ToString();
+                    serialPort1.Open();
+                }
+                catch
+                {
+                    return;
+                }
+
+                if(serialPort1.IsOpen)
+                {
+                    bt_conectar.Text = "Desconectar";
+                    bt_conectar.BackColor = Color.Red;
+                    comboBox1.Enabled = false;
+                }
+            }
+            else
+            {
+                try
+                {
+                    serialPort1.Close();
+                    bt_conectar.Text = "Conectar";
+                    bt_conectar.BackColor = Color.Green;
+                    comboBox1.Enabled = true;
+                }
+                catch
+                {
+                    return;
+                }
+            }
+        }
+
+        private void Bt_enviar_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen == true)
+            {
+                serialPort1.Write(textBox_Enviar.Text);
+                textBox_Enviar.Clear();
+                // quando apertar botao enviar o cursor do mouse precisa voltar para o textbox novamente --'
+            }
+
+            else
+            {
+                MessageBox.Show("Você precisa estar Conectado em uma COM !!!", "Erro",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error,
+                    MessageBoxDefaultButton.Button1);
+            }
+        }
+
+        private void SerialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            Rx_Global = serialPort1.ReadExisting();
+            Invoke(new EventHandler(Recebido));
+        }
+
+        private void Recebido(object sender, EventArgs e)
+        {
+            textBox_Recebido.AppendText(Rx_Global);
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (serialPort1.IsOpen == true)
+            {
+                serialPort1.Close();
+            }
+        }
+        // Quando voce fecha o programa sem desconectar ele fica aberto a COM ainda, se usar outro programa pra ouvir COM não vai conseguir
+
+        private void Limpar_Click(object sender, EventArgs e)
+        {
+            textBox_Recebido.Clear();
+        }
+    }
+}
